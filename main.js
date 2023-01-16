d8d0b3298ad5185d7ae44b9b243c6d5f0dcae588
yuxiaoyujun <xellosssama@sina.com>
2023-01-16 21:23:34 +0800

    feat: 个人简历页面

---
+++ main.js
@@ -0,0 +1,4 @@
+import "./assets/css/devresume.css";
+import "./assets/css/nidai.css";
+import proFileImg from "./assets/images/resume-profile.jpg";
+document.querySelector(".resume-profile-image img").src = proFileImg;
